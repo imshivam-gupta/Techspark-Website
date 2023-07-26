ab79ec81ad35ab29418e0b197715cb120f7b4a56
Shivam Gupta <gupta.shivam2604@gmail.com>
2023-07-26 16:48:31 +0530

    Updated backend

--- utils/dbconnect.js
+++ utils/dbconnect.js
@@ -1,5 +1,5 @@
-// export const BACKEND_URL = 'https://blue-weary-hippo.cyclic.app/'
-export const BACKEND_URL = 'http://localhost:8000/'
+export const BACKEND_URL = 'https://blue-weary-hippo.cyclic.app/'
+// export const BACKEND_URL = 'http://localhost:8000/'
 export const GCLIENTID = '694911048941-4ogcsr662mjmbuusv599sloi8nn60huo.apps.googleusercontent.com'
 
 
